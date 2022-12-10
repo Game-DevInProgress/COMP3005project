5b06d23c3f28b174d4782658dd9960afd357e61d
Tang2525 <120001661+Tang2525@users.noreply.github.com>
2022-12-10 17:47:42 -0500

    Create dml_database.sql

---
+++ SQL/dml_database.sql
@@ -0,0 +1,9 @@
+INSERT INTO Books
+VALUES      ('Percy Jackson',25, 300, 13, 'Adventure', 'Rick Riordan'),
+            ('Maze Runner', 26, 300, 15, 'Adventure', 'James Dashner'),
+            ('Hunger Games' 27, 280, 12, 'Action', 'Suzanne Collins')
+
+INSERT INTO User
+VALUES      ('John Doe', NULL, 'jdoe246', 'password', 'u'),
+            ('Johnny Boomer', NULL, 'jboomer123', 'boomtubes0', 'o')
+
